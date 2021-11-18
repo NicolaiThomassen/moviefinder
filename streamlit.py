e11d5a63f5e4cc5a9df9d659c5ad4ea6fbe6d35f
Nicolai Thomassen <thomassen.nicolai@gmail.com>
2021-11-18 22:30:41 +0100

    changed interface a bit

--- streamlit.py
+++ streamlit.py
@@ -4,14 +4,24 @@ import movieFinder
 
 url = 'https://raw.githubusercontent.com/NicolaiThomassen/moviefinder/main/all_movies.pkl'
 df = pd.read_pickle(url)
+df['genre'] = df[['genre1','genre2','genre3']].fillna("").agg(' '.join, axis=1)
 mf = movieFinder.imdb_moviefinder(df)
 
 rating = st.slider("Minimum average rating", 0.0, 10.0, 6.0, 0.1)
-rating_to = st.slider("Maximum average rating", 0.0, 10.0, 6.0, 0.1)
-num_votes_from = st.slider("Minimum number of votes", 0, 200000, 5000, 1000)
-genre = st.selectbox('Genre', df['genre1'].unique())
-year_from = st.selectbox('Year from', list(range(1990, 2022)))
-year_to = st.selectbox('Year to', list(range(1990, 2022)))
+rating_to = st.slider("Maximum average rating", 0.0, 10.0, 10.0, 0.1)
+num_votes_from = st.slider("Minimum number of votes", 0, 200000, 50000, 1000)
+genres = list(df['genre1'].unique())
+genres.extend((None,))
+idx1 = genres.index(None)
+genre = st.selectbox('Genre', genres, index=idx1)
+yearsa = list(range(1990, 2022, 1))
+yearsa.extend((None,))
+idx2 = yearsa.index(None)
+year_from = st.selectbox('Year from', yearsa, index=idx2)
+yearsb = list(range(2022, 1990, -1))
+yearsb.extend((None,))
+idx3 = yearsb.index(None)
+year_to = st.selectbox('Year to', yearsb, index=idx3)
 
 st.write("Awesome movies", mf.search_movies(
     year_from=year_from,
@@ -21,10 +31,9 @@ st.write("Awesome movies", mf.search_movies(
     average_rating_from=rating,
     average_rating_to=rating_to
 )[
-    ['originalTitle',
+    ['title',
      'averageRating',
+     'genre',
      'numVotes',
-     'genre1',
-     'genre2',
-     'genre3']
-].head(30))
+    'startYear']
+].rename(columns={'title': "Title", "genre": "Genre", 'averageRating':'IMDB Rating', 'numVotes':'Votes', 'startYear':'Year'}).reset_index(drop=True).head(30))
